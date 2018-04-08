66238ce6c44d97d761b757759418ddc55bb01535
brebner <gavin.brebner@orange.fr>
2018-04-08 15:38:34 +0200

    Initial commit

---
+++ app/src/main/java/com/example/brebner/anotherlocationapp/MainActivity.java
@@ -0,0 +1,237 @@
+package com.example.brebner.anotherlocationapp;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private final int REQUEST_LOCATION = 0;
+    private GoogleMap googleMap;
+    private GoogleApiClient googleApiClient;
+    private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (googleServiceAvailable()) {
+            Toast.makeText(this, "Play services OK", Toast.LENGTH_LONG).show();
+            setContentView(R.layout.activity_main);
+            initMap();
+        } else {
+            Toast.makeText(this, "No play services", Toast.LENGTH_LONG).show();
+            return;
+        }
+        Log.d(TAG, "onCreate: setting up location callback");
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                Log.d(TAG, "onLocationResult: started");
+                if (locationResult == null) {
+                    Log.d(TAG, "onLocationResult: Cannot discover location");
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    LatLng ll = new LatLng(location.getLatitude(), location.getLatitude());
+                    CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll, 15);
+                    googleMap.animateCamera(update);
+                    Log.d(TAG, "onLocationResult: setting camera position");
+                }
+            }
+
+            @Override
+            public void onLocationAvailability(LocationAvailability var1) {
+                Log.d(TAG, "onLocationAvailability: ");
+            }
+        };
+    }
+
+    private void initMap() {
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment);
+        mapFragment.getMapAsync(this);
+    }
+
+    public boolean googleServiceAvailable() {
+        GoogleApiAvailability api = GoogleApiAvailability.getInstance();
+        int isAvailable = api.isGooglePlayServicesAvailable(this);
+        if (isAvailable == ConnectionResult.SUCCESS) {
+            return true;
+        } else if (api.isUserResolvableError(isAvailable)) {
+            Dialog dialog = api.getErrorDialog(this, isAvailable, 0);
+            dialog.show();
+        } else {
+            Toast.makeText(this, "Cannot connect to play services", Toast.LENGTH_LONG).show();
+        }
+        return false;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        // gotoLocation(45.1430026,5.8401512);
+        //if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        // TODO: Consider calling
+        //    ActivityCompat#requestPermissions
+        //    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        // here to request the missing permissions, and then overriding
+        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+        //                                          int[] grantResults)
+        // to handle the case where the user grants the permission. See the documentation
+        // for ActivityCompat#requestPermissions for more details.
+        //    return;
+        // }
+        // this.googleMap.setMyLocationEnabled(true);
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        googleApiClient.connect();
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_LOCATION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    this.googleMap.setMyLocationEnabled(true);
+                } else {
+                    Toast.makeText(this, "Permission not granted!!!", Toast.LENGTH_LONG).show();
+                }
+                break;
+            default:
+                break;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void gotoLocation(double lat, double lng) {
+        gotoLocationZoom(lat, lng, 17);
+    }
+
+    private void gotoLocationZoom(double lat, double lng, float zoom) {
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lng), zoom);
+        googleMap.moveCamera(cameraUpdate);
+    }
+
+    public void geoLocate(View view) throws IOException {
+        EditText editText = (EditText) findViewById(R.id.editTextWhere);
+        String location = editText.getText().toString();
+        editText.setText("");
+
+        Geocoder geocoder = new Geocoder(this);
+        List<Address> list = geocoder.getFromLocationName(location, 1);
+        if (list.size() < 1) {
+            Toast.makeText(this, "Unknown Location", Toast.LENGTH_LONG).show();
+        } else {
+            Address address = list.get(0);
+            String locality = address.getLocality();
+            Toast.makeText(this, locality, Toast.LENGTH_SHORT).show();
+            double lat = address.getLatitude();
+            double lng = address.getLongitude();
+            gotoLocationZoom(lat, lng, 17);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuMapTypeNone:
+                this.googleMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+                break;
+            case R.id.menuMapTypeNormal:
+                this.googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                break;
+            case R.id.menuMapTypeSatellite:
+                this.googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                break;
+            case R.id.menuMapTypeTerrain:
+                this.googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                break;
+            case R.id.menuMapTypeHybrid:
+                this.googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        this.locationRequest = new LocationRequest();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(1000);
+
+        FusedLocationProviderClient flpc = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+            return;
+        }
+        flpc.requestLocationUpdates(locationRequest, locationCallback, null);
+    }
+
+
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Toast.makeText(this, "OnConnectionSuspended", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, "OnConnectionFailed", Toast.LENGTH_LONG).show();
+    }
+
+}
